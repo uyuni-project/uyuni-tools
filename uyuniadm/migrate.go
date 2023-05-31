b469d23cb54e2dc03e096170ff07e114b3bbb041
mbussolotto <michele.bussolotto@suse.com>
2023-05-31 15:12:35 +0200

    start and migrate command

---
+++ uyuniadm/migrate.go
@@ -0,0 +1,67 @@
+package main
+
+//TODO required go version >= 18
+//zypper in libbtrfs-devel libgpgme-devel device-mapper-devel gpgme libassuan >= 2.5.3
+//systemct start podman
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/containers/podman/v4/pkg/bindings/containers"
+	"github.com/spf13/cobra"
+)
+
+//TODO avoid global variable
+var source remoteServer
+var sshAuthSocket string
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "migrate command",
+	Run: func(cmd *cobra.Command, arg []string) {
+		cmd.OutOrStdout()
+		cmd.OutOrStderr()
+		setEnvMigrateParam()
+		fmt.Println("Migrating server")
+		opt := readJsonOpt(configJsonFilename)
+		//TODO to allow uyuniadm to handle more than one container, we should find another way to pass the env variable
+		//option from argument and specific for migrate container
+		entrypoint := "/usr/local/bin/uyuni-tools/setup-migration-container.sh"
+		opt.Entrypoint = &entrypoint
+		opt.Name = uyuniContainer.name
+		opt.ConmonPIDFile = opt.Name + ".pid"
+		opt.CIDFile = opt.Name + ".ctr-id"
+		opt.Hostname = "tmp"
+		//TODO we don't want to mount this folder
+		opt.Volume = append(opt.Volume, filepath.Dir(sshAuthSocket)+":"+filepath.Dir(sshAuthSocket))
+		//TODO we don't want to mount this folder
+		opt.Volume = append(opt.Volume, "/usr/local/bin/uyuni-tools/:/usr/local/bin/uyuni-tools/")
+
+		getImage(ctx, image.String())
+		createContainer(opt, image.String())
+
+		startContainer(ctx, opt.Name)
+		if _, err := containers.Wait(ctx, opt.Name, nil); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println("Server migrated")
+	},
+}
+
+//TODO to allow uyuniadm to handle more than one container, we should find another way to pass the env variable
+//TODO do not hardcore env variable
+func setEnvMigrateParam() {
+	os.Setenv("REMOTE_USER", source.user )
+	os.Setenv("UYUNI_FQDN", source.hostname)
+	os.Setenv("CERT_CITY", cert.city)
+	os.Setenv("CERT_COUNTRY", cert.country)
+	os.Setenv("CERT_STATE", cert.state)
+	os.Setenv("CERT_O", cert.org)
+	os.Setenv("CERT_OU", cert.ou)
+	os.Setenv("CERT_PASS", cert.pwd)
+	os.Setenv("CERT_EMAIL", cert.email)
+	os.Setenv("SSH_AUTH_SOCK", sshAuthSocket)
+}
---
+++ uyuniadm/start.go
@@ -0,0 +1,78 @@
+package main
+
+//TODO required go version >= 18
+//zypper in libbtrfs-devel libgpgme-devel device-mapper-devel gpgme libassuan >= 2.5.3
+//systemct start podman
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/containers/podman/v4/pkg/bindings/containers"
+	"github.com/spf13/cobra"
+)
+
+//TODO avoid global variable
+var admin manager
+
+var enableTftp string
+var reportDB database
+
+func startContainer(context context.Context, name string) {
+
+	if err := containers.Start(context, name, nil); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Container started.")
+}
+
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start command",
+	Run: func(cmd *cobra.Command, arg []string) {
+		cmd.OutOrStdout()
+		cmd.OutOrStderr()
+		setEnvStartParam()
+		fmt.Println("Starting container")
+
+		opt := readJsonOpt(configJsonFilename)
+
+		//TODO to allow uyuniadm to handle more than one container, we should find another way to pass the env variable
+		//option from argument and specific for start container
+		opt.Hostname = uyuniContainer.hostname
+		opt.Name = uyuniContainer.name
+		opt.ConmonPIDFile = uyuniContainer.name + ".pid"
+		opt.CIDFile = uyuniContainer.name + ".ctr-id"
+		
+		getImage(ctx, image.String())
+		createContainer(opt, image.String())
+
+		startContainer(ctx, opt.Name)
+	},
+}
+
+//TODO to allow uyuniadm to handle more than one container, we should find another way to pass the env variable
+//TODO do not hardcore env variable
+func setEnvStartParam() {
+	os.Setenv("MANAGER_USER", admin.user)
+	os.Setenv("MANAGER_PASS", admin.password)
+	os.Setenv("MANAGER_ADMIN_EMAIL", admin.email)
+	os.Setenv("MANAGER_DB_NAME", admin.db.name)
+	os.Setenv("MANAGER_DB_HOST", admin.db.host)
+	os.Setenv("MANAGER_DB_PORT", admin.db.port)
+	os.Setenv("MANAGER_DB_PROTOCOL", admin.db.port)
+	os.Setenv("REPORT_DB_HOST", reportDB.host)
+	os.Setenv("REPORT_DB_PASS", reportDB.password)
+	os.Setenv("MANAGER_ENABLE_TFTP", enableTftp)
+	os.Setenv("USE_EXISTING_CERTS", cert.useExistingCertificate)
+	os.Setenv("CERT_CITY", cert.city)
+	os.Setenv("CERT_COUNTRY", cert.country)
+	os.Setenv("CERT_STATE", cert.state)
+	os.Setenv("CERT_O", cert.org)
+	os.Setenv("CERT_OU", cert.ou)
+	os.Setenv("CERT_PASS", cert.pwd)
+	os.Setenv("CERT_EMAIL", cert.email)
+}
+
