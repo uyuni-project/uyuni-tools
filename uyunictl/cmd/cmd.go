f671c3050ffdb470506182233a2672696ff10d2b
Ricardo Mateus <rmateus@suse.com>
2023-09-12 12:59:33 +0100

    Create and configure logger
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- uyunictl/cmd/cmd.go
+++ uyunictl/cmd/cmd.go
@@ -1,14 +1,17 @@
 package cmd
 
 import (
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
+	"github.com/uyuni-project/uyuni-tools/shared/utils"
 	"github.com/uyuni-project/uyuni-tools/uyunictl/cmd/cp"
 	"github.com/uyuni-project/uyuni-tools/uyunictl/cmd/exec"
 )
 
 // NewCommand returns a new cobra.Command implementing the root command for kinder
 func NewUyunictlCommand() *cobra.Command {
+	utils.LogInit("uyunictl")
 	globalFlags := &types.GlobalFlags{}
 	rootCmd := &cobra.Command{
 		Use:     "uyunictl",
@@ -19,6 +22,12 @@ func NewUyunictlCommand() *cobra.Command {
 
 	rootCmd.PersistentFlags().BoolVarP(&globalFlags.Verbose, "verbose", "v", false, "verbose output")
 	rootCmd.PersistentFlags().StringVarP(&globalFlags.ConfigPath, "config", "c", "", "configuration file path")
+	rootCmd.PersistentFlags().StringVar(&globalFlags.LogLevel, "logLevel", "info", "application log level (trace|debug|info|warn|error|fatal|panic)")
+
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		utils.SetLogLevel(globalFlags.LogLevel)
+		log.Info().Msgf("Executing command: %s", cmd.Name())
+	}
 
 	// TODO Add --namespace parameter for kubernetes ?
 
