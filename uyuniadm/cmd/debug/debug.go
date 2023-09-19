b785d46181d8aab1799aedfab9e18a56575be758
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-09-19 10:38:32 +0200

    Rebase on master

--- uyuniadm/cmd/debug/debug.go
+++ uyuniadm/cmd/debug/debug.go
@@ -1,15 +1,17 @@
 package debug
 
 import (
+	"fmt"
 	"log"
+
 	"github.com/spf13/cobra"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
 	"github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 type flagpole struct {
-	Enable			bool
-	Restart			bool
+	Enable  bool
+	Restart bool
 }
 
 func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
@@ -35,19 +37,25 @@ Remote debugging is available using:
 }
 
 func run(globalFlags *types.GlobalFlags, flags *flagpole, cmd *cobra.Command, args []string) {
+	const tomcatFile = "/etc/tomcat/conf.d/remote_debug.conf"
+	const taskoFile = "/etc/rhn/taskomatic.conf"
+
 	if flags.Enable {
+		enableTemplate := "grep -q 'JAVA_OPTS=\" \\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=\\*:%d,server=y,suspend=n\"' %s ||" +
+			" echo 'JAVA_OPTS=\" $JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:%d,server=y,suspend=n\" ' >> %s"
+
 		log.Printf("Enabling remote debug")
-		utils.Exec(globalFlags, "", false, false, []string{}, "grep -q 'JAVA_OPTS=\" \\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=\\*:8000,server=y,suspend=n\"' /etc/tomcat/conf.d/remote_debug.conf || echo 'JAVA_OPTS=\" $JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:8000,server=y,suspend=n\" ' >> /etc/tomcat/conf.d/remote_debug.conf")
-		utils.Exec(globalFlags, "", false, false, []string{}, "grep -q 'JAVA_OPTS=\" \\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=\\*:8001,server=y,suspend=n\"' /etc/rhn/taskomatic.conf || echo 'JAVA_OPTS=\" $JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=*:8001,server=y,suspend=n\" ' >> /etc/rhn/taskomatic.conf")
+		utils.Exec(globalFlags, "", false, false, true, []string{}, fmt.Sprintf(enableTemplate, 8000, tomcatFile, 8000, tomcatFile))
+		utils.Exec(globalFlags, "", false, false, true, []string{}, fmt.Sprintf(enableTemplate, 8001, taskoFile, 8001, taskoFile))
 	} else {
 		log.Printf("Disabling remote debug")
-		utils.Exec(globalFlags, "", false, false, []string{}, "sed -i '/-Xdebug -Xrunjdwp:transport=dt_socket,address=/d' /etc/tomcat/conf.d/remote_debug.conf")
-		utils.Exec(globalFlags, "", false, false, []string{}, "sed -i '/-Xdebug -Xrunjdwp:transport=dt_socket,address=/d' /etc/rhn/taskomatic.conf")
+		utils.Exec(globalFlags, "", false, false, true, []string{}, "sed -i '/-Xdebug -Xrunjdwp:transport=dt_socket,address=/d' "+tomcatFile)
+		utils.Exec(globalFlags, "", false, false, true, []string{}, "sed -i '/-Xdebug -Xrunjdwp:transport=dt_socket,address=/d' "+taskoFile)
 	}
-	
+
 	if flags.Restart {
 		log.Printf("Running spacewalk-service restart. Changes will be applied after restart ")
-		utils.Exec(globalFlags, "", false, false, []string{}, "spacewalk-service restart")
+		utils.Exec(globalFlags, "", false, false, true, []string{}, "spacewalk-service restart")
 	} else {
 		log.Printf("Configuration files changed but not applied. Please restart spacewalk-service to apply them")
 	}
