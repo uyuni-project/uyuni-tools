705dce7dddf3cad54ceb8fc61f9e7a4f24a875b2
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-09-26 06:47:54 +0200

    fixup! Create kubernetes and podman install subcommands

--- uyuniadm/cmd/install/install.go
+++ uyuniadm/cmd/install/install.go
@@ -12,17 +12,7 @@ func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 	installCmd := &cobra.Command{
 		Use:   "install [fqdn]",
 		Short: "install a new server from scratch",
-		Long: `Install a new server from scratch
-
-The install command assumes the following:
-  * podman or kubectl is installed locally
-  * if kubectl is installed, a working kubeconfig should be set to connect to the cluster to deploy to
-
-When installing on kubernetes, the helm values file will be overridden with the values from the uyuniadm parameters or configuration.
-
-NOTE: for now installing on a remote cluster or podman is not supported!
-`,
-		Args: cobra.ExactArgs(1),
+		Long:  "Install a new server from scratch",
 	}
 
 	installCmd.AddCommand(podman.NewCommand(globalFlags))
