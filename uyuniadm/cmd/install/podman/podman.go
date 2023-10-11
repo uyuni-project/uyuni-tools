68a00ae0cbdd45475bff29da4ed648801499815e
Ricardo Mateus <rmateus@suse.com>
2023-10-11 17:04:28 +0100

    make fqdn optional for podman install
    Signed-off-by: Ricardo Mateus <rmateus@suse.com>

--- uyuniadm/cmd/install/podman/podman.go
+++ uyuniadm/cmd/install/podman/podman.go
@@ -25,7 +25,7 @@ The install podman command assumes podman is installed locally
 
 NOTE: for now installing on a remote podman is not supported!
 `,
-		Args: cobra.ExactArgs(1),
+		Args: cobra.MaximumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			viper := utils.ReadConfig(globalFlags.ConfigPath, "admconfig", cmd)
 			var flags podmanInstallFlags
