5061b5b4ccd8f877960a7de04c3f7d8af0e66aa7
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-10-11 14:59:48 +0200

    Change wait commands to Trace level rather than Debug

--- uyuniadm/shared/kubernetes/k3s.go
+++ uyuniadm/shared/kubernetes/k3s.go
@@ -31,7 +31,7 @@ func InstallK3sTraefikConfig(debug bool) {
 	// Wait for traefik to be back
 	log.Info().Msg("Waiting for Traefik to be reloaded")
 	for i := 0; i < 60; i++ {
-		out, err := utils.RunCmdOutput(zerolog.DebugLevel, "kubectl", "get", "job", "-A",
+		out, err := utils.RunCmdOutput(zerolog.TraceLevel, "kubectl", "get", "job", "-A",
 			"-o", "jsonpath={.status.completionTime}", "helm-install-traefik")
 		if err == nil {
 			completionTime, err := time.Parse(time.RFC3339, string(out))
