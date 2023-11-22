1e5eb8d3e85ad817334d9ba0c44ec52bb6e06d4e
Ondrej Holecek <oholecek@suse.com>
2023-11-22 15:23:25 +0100

    Use viper to deserialize API options

--- uyunictl/cmd/api/api.go
+++ uyunictl/cmd/api/api.go
@@ -22,7 +22,7 @@ func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 		Long:  "Takes an API path and optional parameters and then issues GET request with the specified path and parameters. If user and password are provided, calls login before API call",
 		Run: func(cmd *cobra.Command, args []string) {
 			viper := utils.ReadConfig(globalFlags.ConfigPath, "ctlconfig", cmd)
-			if err := viper.Unmarshal(&globalFlags); err != nil {
+			if err := viper.Unmarshal(&apiFlags); err != nil {
 				log.Fatal().Err(err).Msgf("Failed to unmarshall configuration")
 			}
 			runGet(globalFlags, apiFlags, cmd, args)
@@ -35,7 +35,7 @@ func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 		Long:  "Takes an API path and parameters and then issues POST request with the specified path and parameters. User and password are mandatory. Parameters can be either JSON encoded string or one or more key=value pairs.",
 		Run: func(cmd *cobra.Command, args []string) {
 			viper := utils.ReadConfig(globalFlags.ConfigPath, "ctlconfig", cmd)
-			if err := viper.Unmarshal(&globalFlags); err != nil {
+			if err := viper.Unmarshal(&apiFlags); err != nil {
 				log.Fatal().Err(err).Msgf("Failed to unmarshall configuration")
 			}
 			runPost(globalFlags, apiFlags, cmd, args)
@@ -45,6 +45,6 @@ func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 	apiCmd.AddCommand(apiGet)
 	apiCmd.AddCommand(apiPost)
 
-	api.AddAPIFlags(apiCmd, apiFlags, false)
+	api.AddAPIFlags(apiCmd, false)
 	return apiCmd
 }
