7b3b715704097b535027cb4426f43a77e905d916
Cédric Bosdonnat <cbosdonnat@suse.com>
2023-12-08 16:34:21 +0100

    Create a helper to share the --pullPolicy flag between mgradm and mgrpxy
    Since the proxy has multiple images we don't want the user to set the
    pull policy for each of them, but globally. This requires extracting
    that code in a separate helper function.

--- shared/utils/cmd.go
+++ shared/utils/cmd.go
@@ -17,3 +17,17 @@ var Version = "0.0.0"
 func AddBackendFlag(cmd *cobra.Command) {
 	cmd.Flags().String("backend", "", "tool to use to reach the container. Possible values: 'podman', 'podman-remote', 'kubectl'. Default guesses which to use.")
 }
+
+// AddPullPolicyFlag adds the --pullPolicy flag to a command.
+//
+// Since podman doesn't have such a concept of pull policy like kubernetes,
+// the values need some explanations for it:
+//   - Never: just check and fail if needed
+//   - IfNotPresent: check and pull
+//   - Always: pull without checking
+//
+// For kubernetes the value is simply passed to the helm charts.
+func AddPullPolicyFlag(cmd *cobra.Command) {
+	cmd.Flags().String("pullPolicy", "IfNotPresent",
+		"set whether to pull the images or not. The value can be one of 'Never', 'IfNotPresent' or 'Always'")
+}
