uyuni-project/uyuni-tools

commit 85d09cd461eb4a12da5892427b5001b5eb08b1ba

Pass the TERM env to mgrctl exec
Podman sets a dumb TERM value that breaks some tools like less. Setting
the TERM for the terminal in interactive mode fixes it.

mgrctl/cmd/exec/exec.go
@@ -60,11 +60,13 @@ func run(flags *flagpole, cmd *cobra.Command, args []string) {
 	}
 
 	commandArgs := []string{"exec"}
+	envs := []string{}
 	if flags.Interactive {
 		commandArgs = append(commandArgs, "-i")
 	}
 	if flags.Tty {
 		commandArgs = append(commandArgs, "-t")
+		envs = append(envs, "TERM")
 	}
 	commandArgs = append(commandArgs, podName)
 
@@ -73,7 +75,7 @@ func run(flags *flagpole, cmd *cobra.Command, args []string) {
 	}
 
 	newEnv := []string{}
-	for _, envValue := range flags.Envs {
+	for _, envValue := range envs {
 		if !strings.Contains(envValue, "=") {
 			if value, set := os.LookupEnv(envValue); set {
 				newEnv = append(newEnv, fmt.Sprintf("%s=%s", envValue, value))

commit 320f5c273a0c8dca9e1bef3fd04e3a4093cbcaed

Fix environment variables handling in mgrctl exec
Commit 85d09cd introduced a regression by forgetting to add the passed
environment variables to the command.

mgrctl/cmd/exec/exec.go
@@ -61,6 +61,7 @@ func run(flags *flagpole, cmd *cobra.Command, args []string) {
 
 	commandArgs := []string{"exec"}
 	envs := []string{}
+	envs = append(envs, flags.Envs...)
 	if flags.Interactive {
 		commandArgs = append(commandArgs, "-i")
 	}