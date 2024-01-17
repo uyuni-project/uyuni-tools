5af59aebb4cf50b38725a23eb645f7954b0159b0
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-01-17 15:40:39 +0100

    Fix version calculation in build.sh to match tito's tags
    tito names tags differently than what is implemented in build.sh, adapt
    the script now that we have some of those tags.

--- build.sh
+++ build.sh
@@ -6,8 +6,9 @@
 
 mkdir -p ./bin
 
-tag=$(git describe --tags --abbrev=0 | cut -f 3 -d '-')
-offset=$(git rev-list --count ${tag})
+tag=$(git describe --tags --abbrev=0)
+version=$(git describe --tags --abbrev=0 | cut -f 3 -d '-')
+offset=$(git rev-list --count ${tag}..)
 
 VERSION_NAME=github.com/uyuni-project/uyuni-tools/shared/utils.Version
 
