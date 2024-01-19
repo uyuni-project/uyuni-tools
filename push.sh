2e9f0274039e813b24467784f7d0489280dc536f
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-01-19 13:38:45 +0100

    Add push script to tweak spec file for IBS
    There are some build differences between the tools for Uyuni and those
    for SUSE Manager. Add a push.sh script to apply those changes when
    pushing to IBS for SUSE Manager.

---
+++ push.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# SPDX-FileCopyrightText: 2024 SUSE LLC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# This script is called by push-packages-to-obs
+
+OSCAPI=$1
+GIT_DIR=$2
+PKG_NAME=$3
+
+SRPM_PKG_DIR=$(dirname "$0")
+
+if [ "${OSCAPI}" == "https://api.suse.de" ]; then
+  sed 's/^tag=%{!?_default_tag:latest}/tag=5.0.0-alpha1/' -i ${SRPM_PKG_DIR}/uyuni-tools.spec
+fi
