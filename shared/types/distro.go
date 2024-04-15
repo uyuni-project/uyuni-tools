94590ecb8b01e259aa4507bc6e621b748c9554e6
Ondrej Holecek <oholecek@suse.com>
2024-04-15 15:25:04 +0200

    Refactor distro.detect
    - product map now containes 3rd level for arch instead of being part of
      final struct
    - prepare distro data also from command line provided details
    - add arch constants

--- shared/types/distro.go
+++ shared/types/distro.go
@@ -10,5 +10,34 @@ type Distribution struct {
 	BasePath     string
 	ChannelLabel string
 	InstallType  string
-	Arch         string
+}
+
+type DistributionDetails struct {
+	Name    string
+	Version string
+	Arch    Arch
+}
+
+type Arch string
+
+const (
+	UnknownArch Arch = "unknown"
+	AMD64       Arch = "x86_64"
+	AArch64     Arch = "aarch64"
+	S390X       Arch = "s390x"
+	PPC64LE     Arch = "ppc64le"
+)
+
+func GetArch(a string) Arch {
+	switch a {
+	case "x86_64":
+		return AMD64
+	case "aarch64":
+		return AArch64
+	case "s390x":
+		return S390X
+	case "ppc64le":
+		return PPC64LE
+	}
+	return UnknownArch
 }
