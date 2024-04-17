ddc64bf2e3151399607882c703f49523bb77a23a
Michele Bussolotto <michele.bussolotto@suse.com>
2024-04-17 10:12:35 +0200

    add gpg command (#216)
    * add import gpg command
    
    Co-authored-by: Cedric Bosdonnat <cbosdonnat@suse.com>
    
    * import-minimal gpg key
    
    * restart uyuni-update-config to update gpg key
    
    * add localization
    
    ---------
    
    Co-authored-by: Cedric Bosdonnat <cbosdonnat@suse.com>

---
+++ mgradm/cmd/gpg/gpg.go
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2024 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gpg
+
+import (
+	"github.com/spf13/cobra"
+	gpgadd "github.com/uyuni-project/uyuni-tools/mgradm/cmd/gpg/add"
+	"github.com/uyuni-project/uyuni-tools/shared/types"
+)
+
+// NewCommand import gpg keys from 3rd party repository.
+func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
+	gpgKeyCmd := &cobra.Command{
+		Use:   "gpg",
+		Short: "Manage gpg keys for 3rd party repositories",
+		Args:  cobra.ExactArgs(1),
+	}
+
+	gpgKeyCmd.AddCommand(gpgadd.NewCommand(globalFlags))
+
+	return gpgKeyCmd
+}
