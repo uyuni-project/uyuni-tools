1aadb43041d7149182f87731fe7b73bc4e99f3af
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-05-06 09:38:28 +0200

    Fix Ubuntu builds

--- push.sh
+++ push.sh
@@ -44,4 +44,4 @@ else
 fi
 
 # Add the version_details value for use in the version tag
-sed "/^%global productname.*$/a%global version_details '${VERSION} $COMMIT_ID'" -i ${SRPM_PKG_DIR}/uyuni-tools.spec
+sed "/^%global productname.*$/a%global version_details ${VERSION} $COMMIT_ID" -i ${SRPM_PKG_DIR}/uyuni-tools.spec
