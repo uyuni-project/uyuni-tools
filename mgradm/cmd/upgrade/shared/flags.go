279db46c76766fdefb81ceda1a744cc295f09ed1
mbussolotto <michele.bussolotto@suse.com>
2024-05-27 16:41:16 +0200

    use always the same pull policy

--- mgradm/cmd/upgrade/shared/flags.go
+++ mgradm/cmd/upgrade/shared/flags.go
@@ -18,11 +18,11 @@ type UpgradeFlags struct {
 
 // AddUpgradeFlags add upgrade flags to a command.
 func AddUpgradeFlags(cmd *cobra.Command) {
-	utils.AddImageUpgradeFlag(cmd)
+	utils.AddImageFlag(cmd)
 	utils.AddDbUpgradeImageFlag(cmd)
 }
 
 // AddUpgradeListFlags add upgrade list flags to a command.
 func AddUpgradeListFlags(cmd *cobra.Command) {
-	utils.AddImageUpgradeFlag(cmd)
+	utils.AddImageFlag(cmd)
 }
