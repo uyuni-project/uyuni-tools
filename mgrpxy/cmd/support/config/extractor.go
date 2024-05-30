bdab5306f90391dccb4c3271508d473e2df6d239
mbussolotto <michele.bussolotto@suse.com>
2024-05-30 17:24:30 +0200

    save systemd configuration in supportconfig

--- mgrpxy/cmd/support/config/extractor.go
+++ mgrpxy/cmd/support/config/extractor.go
@@ -5,55 +5,50 @@
 package config
 
 import (
+	"os"
+
 	"github.com/spf13/cobra"
-	"github.com/uyuni-project/uyuni-tools/shared"
-	"github.com/uyuni-project/uyuni-tools/shared/kubernetes"
-	"github.com/uyuni-project/uyuni-tools/shared/podman"
+	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
 	"github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 func extract(globalFlags *types.GlobalFlags, flags *configFlags, cmd *cobra.Command, args []string) error {
-	var allContainerSupportConfigFiles []string
-
-	for cnx := range getAllProxyCnx(flags.Backend) {
-		containerSupportConfigFiles, err := shared.RunSupportConfig(cnx)
-		if err != nil {
-			return err
-		}
-		allContainerSupportConfigFiles = append(allContainerSupportConfigFiles, containerSupportConfigFiles...)
+	// Copy the generated file locally
+	tmpDir, err := os.MkdirTemp("", "mgrpxy-*")
+	if err != nil {
+		return utils.Errorf(err, L("failed to create temporary directory"))
 	}
+	defer os.RemoveAll(tmpDir)
 
-	hostSupportConfigFiles, err := utils.RunSupportConfigOnHost()
+	hostSupportConfigFiles, err := utils.RunSupportConfigOnHost(tmpDir)
 	if err != nil {
 		return err
 	}
 
-	allContainerSupportConfigFiles = append(allContainerSupportConfigFiles, hostSupportConfigFiles...)
-
 	// TODO Get cluster infos in case of kubernetes
 
-	if err := utils.CreateSupportConfigTarball(flags.Output, allContainerSupportConfigFiles); err != nil {
+	if err := utils.CreateSupportConfigTarball(flags.Output, hostSupportConfigFiles); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func getAllProxyCnx(backend string) map[*shared.Connection]bool {
-	cnxs := make(map[*shared.Connection]bool)
-
-	/* this is as hack but it works. We loop for podman proxy container name since the proxy filter for kubernetes
-	* is just one. Storing the context in a unique list, we would have one result if kubernetes and one for each
-	* container for podman.
-	 */
-	for _, container := range podman.ProxyContainerNames {
-		cnx := shared.NewConnection(backend, container, kubernetes.ProxyFilter)
-		if cnxs[cnx] {
-			continue
-		} else {
-			cnxs[cnx] = true
-		}
-	}
-	return cnxs
-}
+//func getAllProxyCnx(backend string) map[*shared.Connection]bool {
+//	cnxs := make(map[*shared.Connection]bool)
+//
+//	/* this is as hack but it works. We loop for podman proxy container name since the proxy filter for kubernetes
+//	* is just one. Storing the context in a unique list, we would have one result if kubernetes and one for each
+//	* container for podman.
+//	 */
+//	for _, container := range podman.ProxyContainerNames {
+//		cnx := shared.NewConnection(backend, container, kubernetes.ProxyFilter)
+//		if cnxs[cnx] {
+//			continue
+//		} else {
+//			cnxs[cnx] = true
+//		}
+//	}
+//	return cnxs
+//}
