e77e816822ba0ee231d660413c1f07611c92631d
Ricardo Mestre <ricardo.mestre@suse.com>
2024-06-07 16:29:20 +0100

    Add mgrpxy cache clear command
    A command to help cleaning the squid cache on the proxy would be quite
    appreciated as this is the best way to fix some issues.

--- mgradm/cmd/status/kubernetes.go
+++ mgradm/cmd/status/kubernetes.go
@@ -26,24 +26,14 @@ func kubernetesStatus(
 	cmd *cobra.Command,
 	args []string,
 ) error {
-	// Do we have an uyuni helm release?
-	clusterInfos, err := kubernetes.CheckCluster()
-	if err != nil {
-		return utils.Errorf(err, L("failed to discover the cluster type"))
-	}
-
-	kubeconfig := clusterInfos.GetKubeconfig()
-	if !kubernetes.HasHelmRelease("uyuni", kubeconfig) {
-		return errors.New(L("no uyuni helm release installed on the cluster"))
-	}
-
-	namespace, err := kubernetes.FindNamespace("uyuni", kubeconfig)
+	cnx := shared.NewConnection("kubectl", "", kubernetes.ServerFilter)
+	namespace, err := cnx.GetNamespace("")
 	if err != nil {
 		return utils.Errorf(err, L("failed to find the uyuni deployment namespace"))
 	}
 
 	// Is the pod running? Do we have all the replicas?
-	status, err := kubernetes.GetDeploymentStatus(namespace, "uyuni")
+	status, err := kubernetes.GetDeploymentStatus(namespace, kubernetes.ServerApp)
 	if err != nil {
 		return utils.Errorf(err, L("failed to get deployment status"))
 	}
@@ -56,7 +46,6 @@ func kubernetesStatus(
 	}
 
 	// Are the services running in the container?
-	cnx := shared.NewConnection("kubectl", "", kubernetes.ServerFilter)
 	if err := adm_utils.ExecCommand(zerolog.InfoLevel, cnx, "spacewalk-service", "status"); err != nil {
 		return utils.Errorf(err, L("failed to run spacewalk-service status"))
 	}
