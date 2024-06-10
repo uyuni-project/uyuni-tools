6235b835db98f48b35a9d6b751fce3eccdc45142
Ondrej Holecek <oholecek@suse.com>
2024-06-10 21:56:58 +0200

    Try to reuse existing user/org if it is possible

--- mgradm/cmd/install/shared/shared.go
+++ mgradm/cmd/install/shared/shared.go
@@ -5,6 +5,8 @@
 package shared
 
 import (
+	"errors"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -49,10 +51,37 @@ func RunSetup(cnx *shared.Connection, flags *InstallFlags, fqdn string, env map[
 		apiCnx := api.ConnectionDetails{
 			Server:   fqdn,
 			Insecure: true, // TODO Get the CA Cert and toggle this to false
+			User:     flags.Admin.Login,
+			Password: flags.Admin.Password,
 		}
-		_, err := org.CreateFirst(&apiCnx, flags.Organization, &flags.Admin)
-		if err != nil {
-			return err
+
+		// Check if there is already admin user with given password and organization with same name
+		if _, err := api.Init(&apiCnx); err == nil {
+			if _, err := org.GetOrganizationDetails(&apiCnx, flags.Organization); err == nil {
+				log.Info().Msgf(L("Server organization already exists, reusing"))
+			} else {
+				log.Debug().Err(err).Msg("Error returned by server")
+				log.Warn().Msgf(L("Administration user already exists, but organization %s could not be found"), flags.Organization)
+			}
+		} else {
+			var connError *url.Error
+			if errors.As(err, &connError) {
+				// We were not able to connect to the server at all
+				return err
+			}
+			// We do not have any user existing, do not try to login
+			apiCnx = api.ConnectionDetails{
+				Server:   fqdn,
+				Insecure: true, // TODO Get the CA Cert and toggle this to false
+			}
+			_, err := org.CreateFirst(&apiCnx, flags.Organization, &flags.Admin)
+			if err != nil {
+				if preconfigured {
+					log.Warn().Msgf(L("Administration user already exists, but provided credentials are not valid"))
+				} else {
+					return err
+				}
+			}
 		}
 	}
 
