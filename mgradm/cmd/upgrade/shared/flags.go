0a5b418770f4d762380e4a3ac025595d3a0ef830
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-06-24 15:36:32 +0200

    Add confidential computing default image in the help

--- mgradm/cmd/upgrade/shared/flags.go
+++ mgradm/cmd/upgrade/shared/flags.go
@@ -10,6 +10,7 @@ import (
 	"github.com/uyuni-project/uyuni-tools/mgradm/shared/utils"
 	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
+	shared_utils "github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 // UpgradeFlags represents flags used for upgrading a server.
@@ -23,7 +24,12 @@ type UpgradeFlags struct {
 func AddUpgradeFlags(cmd *cobra.Command) {
 	utils.AddImageFlag(cmd)
 	utils.AddDbUpgradeImageFlag(cmd)
-	utils.AddContainerImageFlags(cmd, "coco", L("confidential computing attestation"))
+
+	_ = shared_utils.AddFlagHelpGroup(cmd, &shared_utils.Group{
+		ID:    "coco-container",
+		Title: L("Confidential Computing Flags"),
+	})
+	utils.AddContainerImageFlags(cmd, "coco", L("confidential computing attestation"), "coco-container", "server-attestation")
 }
 
 // AddUpgradeListFlags add upgrade list flags to a command.
