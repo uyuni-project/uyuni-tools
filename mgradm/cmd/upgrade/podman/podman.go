1e24343cc991eb39afc848484765006a1c1109e1
mbussolotto <michele.bussolotto@suse.com>
2024-07-18 15:55:51 +0200

    show output of podman image search --list-tags command

--- mgradm/cmd/upgrade/podman/podman.go
+++ mgradm/cmd/upgrade/podman/podman.go
@@ -45,10 +45,8 @@ func NewCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 			if err := viper.Unmarshal(&flags); err != nil {
 				log.Fatal().Err(err).Msg(L("failed to unmarshall configuration"))
 			}
-			tags, _ := podman.ShowAvailableTag(globalFlags.Registry, flags.Image)
-			log.Info().Msgf(L("Available Tags for image: %s"), flags.Image.Name)
-			for _, value := range tags {
-				log.Info().Msgf(value)
+			if err := podman.ShowAvailableTag(globalFlags.Registry, flags.Image); err != nil {
+				log.Fatal().Err(err)
 			}
 		},
 	}
