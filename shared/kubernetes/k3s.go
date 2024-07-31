3272f223ae6834b04c88b797bd1dee723d866d92
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-07-31 18:27:44 +0200

    Properly uninstall traefik configuration
    When removing the configuration file traefik is not reinstalled. This
    can be achieved by change the file to an empty one and deleting it once
    traefik has been reinstalled.

--- shared/kubernetes/k3s.go
+++ shared/kubernetes/k3s.go
@@ -33,6 +33,10 @@ func InstallK3sTraefikConfig(tcpPorts []types.PortMap, udpPorts []types.PortMap)
 	}
 
 	// Wait for traefik to be back
+	waitForTraefik()
+}
+
+func waitForTraefik() {
 	log.Info().Msg(L("Waiting for Traefik to be reloaded"))
 	for i := 0; i < 60; i++ {
 		out, err := utils.RunCmdOutput(zerolog.TraceLevel, "kubectl", "get", "job", "-n", "kube-system",
@@ -48,6 +52,21 @@ func InstallK3sTraefikConfig(tcpPorts []types.PortMap, udpPorts []types.PortMap)
 
 // UninstallK3sTraefikConfig uninstall K3s Traefik configuration.
 func UninstallK3sTraefikConfig(dryRun bool) {
+	// Write a blank file first to get traefik to be reinstalled
+	if !dryRun {
+		log.Info().Msg(L("Reinstalling Traefik without additionnal configuration"))
+		err := os.WriteFile(k3sTraefikConfigPath, []byte{}, 0600)
+		if err != nil {
+			log.Error().Err(err).Msg(L("failed to write empty traefik configuration"))
+		} else {
+			// Wait for traefik to be back
+			waitForTraefik()
+		}
+	} else {
+		log.Info().Msg(L("Would reinstall Traefik without additionnal configuration"))
+	}
+
+	// Now that it's reinstalled, remove the file
 	utils.UninstallFile(k3sTraefikConfigPath, dryRun)
 }
 
