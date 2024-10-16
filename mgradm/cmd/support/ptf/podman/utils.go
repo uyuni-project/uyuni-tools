36535e6d8a2607adf203a835df4a9c19caa7e0ac
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-10-16 17:47:47 +0200

    ptf: check for coco and hub images to be available
    If the coco or hub image is built for a PTF, then we probably need to
    install it.

--- mgradm/cmd/support/ptf/podman/utils.go
+++ mgradm/cmd/support/ptf/podman/utils.go
@@ -11,7 +11,6 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/uyuni-project/uyuni-tools/mgradm/shared/podman"
-	adm_utils "github.com/uyuni-project/uyuni-tools/mgradm/shared/utils"
 	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
 	podman_shared "github.com/uyuni-project/uyuni-tools/shared/podman"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
@@ -24,15 +23,7 @@ func ptfForPodman(
 	cmd *cobra.Command,
 	args []string,
 ) error {
-	//we don't want to perform a postgres version upgrade when installing a PTF.
-	//in that case, we can use the upgrade command.
-	dummyImage := types.ImageFlags{}
-	dummyCoco := adm_utils.CocoFlags{}
-	dummyHubXmlrpc := adm_utils.HubXmlrpcFlags{}
-	if err := flags.checkParameters(); err != nil {
-		return err
-	}
-
+	// Login first to be able to search the registry for PTF images
 	hostData, err := podman_shared.InspectHost()
 	if err != nil {
 		return err
@@ -44,8 +35,19 @@ func ptfForPodman(
 	}
 	defer cleaner()
 
-	return podman.Upgrade(authFile, "", flags.Image, dummyImage, dummyCoco, dummyHubXmlrpc)
+	//we don't want to perform a postgres version upgrade when installing a PTF.
+	//in that case, we can use the upgrade command.
+	dummyImage := types.ImageFlags{}
+	if err := flags.checkParameters(); err != nil {
+		return err
+	}
+
+	return podman.Upgrade(authFile, "", flags.Image, dummyImage, flags.Coco, flags.Hubxmlrpc)
 }
+
+// variables for unit testing.
+var getServiceImage = podman_shared.GetServiceImage
+var hasRemoteImage = podman_shared.HasRemoteImage
 
 func (flags *podmanPTFFlags) checkParameters() error {
 	if flags.TestId != "" && flags.PTFId != "" {
@@ -57,10 +59,6 @@ func (flags *podmanPTFFlags) checkParameters() error {
 	if flags.CustomerId == "" {
 		return errors.New(L("user flag cannot be empty"))
 	}
-	serverImage, err := podman_shared.GetRunningImage(podman_shared.ServerContainerName)
-	if err != nil {
-		return err
-	}
 
 	suffix := "ptf"
 	projectId := flags.PTFId
@@ -68,10 +66,43 @@ func (flags *podmanPTFFlags) checkParameters() error {
 		suffix = "test"
 		projectId = flags.TestId
 	}
+
+	serverImage := getServiceImage(podman_shared.ServerService)
+	if serverImage == "" {
+		return errors.New(L("failed to find server image"))
+	}
+
+	var err error
+
 	flags.Image.Name, err = utils.ComputePTF(flags.CustomerId, projectId, serverImage, suffix)
 	if err != nil {
 		return err
 	}
-	log.Info().Msgf(L("The image computed is: %s"), flags.Image.Name)
+	log.Info().Msgf(L("The computed image is %s"), flags.Image.Name)
+
+	if cocoImage := getServiceImage(podman_shared.ServerAttestationService + "@"); cocoImage != "" {
+		// If no coco image was found then skip it during the upgrade.
+		cocoImage, err = utils.ComputePTF(flags.CustomerId, projectId, cocoImage, suffix)
+		if err != nil {
+			return err
+		}
+		if hasRemoteImage(cocoImage) {
+			flags.Coco.Image.Name = cocoImage
+			log.Info().Msgf(L("The computed confidential computing image is %s"), flags.Coco.Image.Name)
+		}
+	}
+
+	if hubImage := getServiceImage(podman_shared.HubXmlrpcService); hubImage != "" {
+		// If no hub xmlrpc api image was found then skip it during the upgrade.
+		hubImage, err = utils.ComputePTF(flags.CustomerId, projectId, hubImage, suffix)
+		if err != nil {
+			return err
+		}
+		if hasRemoteImage(hubImage) {
+			flags.Hubxmlrpc.Image.Name = hubImage
+			log.Info().Msgf(L("The computed hub XML-RPC API image is %s"), flags.Hubxmlrpc.Image.Name)
+		}
+	}
+
 	return nil
 }
