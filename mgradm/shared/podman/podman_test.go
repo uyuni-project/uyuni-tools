ee179f31324b16f9ef806883ef61cc13c91a6dd3
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-10-18 17:09:04 +0200

    Update the server systemd service during upgrade
    Fixing the podman arguments of the server at upgrade requires updating
    the systemd service itself.

---
+++ mgradm/shared/podman/podman_test.go
@@ -0,0 +1,77 @@
+// SPDX-FileCopyrightText: 2024 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package podman
+
+import (
+	"testing"
+
+	"github.com/uyuni-project/uyuni-tools/shared/test_utils"
+)
+
+func TestHasDebugPorts(t *testing.T) {
+	data := map[string]bool{
+		`[Service]
+ExecStart=/bin/sh -c '/usr/bin/podman run \
+        --name uyuni-server \
+        --hostname uyuni-server.mgr.internal \
+        --rm --cap-add NET_RAW --tmpfs /run -v cgroup:/sys/fs/cgroup:rw \
+        -p 80:80 \
+        -p [::]:80:80 \
+        -p 8003:8003 \
+        -p [::]:8003:8003 \
+        -p 4505:4505 \
+        -p [::]:4505:4505`: true,
+		`[Service]
+ExecStart=/bin/sh -c '/usr/bin/podman run \
+        --name uyuni-server \
+        --hostname uyuni-server.mgr.internal \
+        --rm --cap-add NET_RAW --tmpfs /run -v cgroup:/sys/fs/cgroup:rw \
+        -p 80:80 \
+        -p [::]:80:80 \
+        -p [::]:8003:8003 \
+        -p 4505:4505 \
+        -p [::]:4505:4505`: false,
+	}
+
+	for definition, expected := range data {
+		actual := hasDebugPorts([]byte(definition))
+		test_utils.AssertEquals(t, "Unexpected result for "+definition, expected, actual)
+	}
+}
+
+func TestGetMirrorPath(t *testing.T) {
+	data := map[string]string{
+		`[Service]
+ExecStart=/bin/sh -c '/usr/bin/podman run \
+        --name uyuni-server \
+        --hostname uyuni-server.mgr.internal \
+        --rm --cap-add NET_RAW --tmpfs /run -v cgroup:/sys/fs/cgroup:rw \
+        -p 80:80 \
+        -p 4505:4505 \
+        -p [::]:4505:4505`: "",
+		`[Service]
+ExecStart=/bin/sh -c '/usr/bin/podman run \
+        --name uyuni-server \
+        --hostname uyuni-server.mgr.internal \
+        --rm --cap-add NET_RAW --tmpfs /run -v cgroup:/sys/fs/cgroup:rw \
+		-v   /path/to/mirror:/mirror:z \
+        -p 80:80 \
+        -p 4505:4505 \
+        -p [::]:4505:4505`: "/path/to/mirror",
+		`[Service]
+ExecStart=/bin/sh -c '/usr/bin/podman run \
+        --name uyuni-server \
+        --hostname uyuni-server.mgr.internal \
+		--rm --cap-add NET_RAW -v /path/to/mirror:/mirror --tmpfs /run -v cgroup:/sys/fs/cgroup:rw \
+        -p 80:80 \
+        -p 4505:4505 \
+        -p [::]:4505:4505`: "/path/to/mirror",
+	}
+
+	for definition, expected := range data {
+		actual := getMirrorPath([]byte(definition))
+		test_utils.AssertEquals(t, "Unexpected result for "+definition, expected, actual)
+	}
+}
