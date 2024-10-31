3688a9307aa4fbced5c2cd57e4a8820382b96037
Cédric Bosdonnat <cbosdonnat@suse.com>
2024-10-31 11:38:35 +0100

    Add proxy config unit tests and rename the flags
    Change the proxy config command to unit test that its flags are parsed
    correctly into the corresponding data structures.
    
    While at it also rename the flags to ease the grouping in the configuration
    file and reusing the other code.
    
    So --proxyName, --proxyPort, --server, --maxCache and --email are renamed to
    --proxy-name, --proxy-sshPort, --proxy-parent, --proxy-maxCache and
    --proxy-email and could be passed as configuration like:
    
        proxy:
          name: pxy1.test.com
          sshPort: 8022
          parent: uyuni.test.com
          maxCache: 1234
          email: pxy1@test.com
    
    Along the same line the SSL flags are renamed to start with `--ssl-` and
    be shared as much as possible with those from mgradm install and migrate commands.

--- mgrctl/cmd/proxy/config.go
+++ mgrctl/cmd/proxy/config.go
@@ -8,121 +8,119 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/uyuni-project/uyuni-tools/shared/api"
 	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
+	"github.com/uyuni-project/uyuni-tools/shared/ssl"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
 	"github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 // Specific flag names for proxy create config command.
 const (
-	proxyName       = "proxyName"
-	proxyPort       = "proxyPort"
-	server          = "server"
-	maxCache        = "maxCache"
-	email           = "email"
-	output          = "output"
-	caCrt           = "caCrt"
-	caKey           = "caKey"
-	caPassword      = "caPassword"
-	cNames          = "cnames"
-	country         = "country"
-	state           = "state"
-	city            = "city"
-	org             = "org"
-	orgUnit         = "orgUnit"
-	sslEmail        = "sslEmail"
-	intermediateCAs = "intermediateCAs"
-	proxyCrt        = "proxyCrt"
-	proxyKey        = "proxyKey"
+	proxyName      = "proxy-name"
+	proxyPort      = "proxy-sshPort"
+	server         = "proxy-parent"
+	maxCache       = "proxy-maxCache"
+	email          = "proxy-email"
+	output         = "output"
+	caCrt          = "ssl-ca-cert"
+	caKey          = "ssl-ca-key"
+	caPassword     = "ssl-ca-password"
+	caIntermediate = "ssl-ca-intermediate"
+	proxyCrt       = "ssl-proxy-cert"
+	proxyKey       = "ssl-proxy-key"
+	sslEmail       = "ssl-email"
 )
+
+type proxyFlags struct {
+	Name     string
+	Port     int `mapstructure:"sshPort"`
+	Parent   string
+	MaxCache int
+	Email    string
+}
+
+type caFlags struct {
+	types.SslPair `mapstructure:",squash"`
+	Password      string
+	Intermediate  []string
+}
+
+type proxyConfigSslFlags struct {
+	types.SslCertGenerationFlags `mapstructure:",squash"`
+	Proxy                        types.SslPair
+	Ca                           caFlags
+}
 
 // proxyCreateConfigFlags is the structure containing the flags for proxy create config command.
 type proxyCreateConfigFlags struct {
 	ConnectionDetails api.ConnectionDetails `mapstructure:"api"`
-	ProxyName         string
-	ProxyPort         int
-	Server            string
-	MaxCache          int
-	Email             string
+	Proxy             proxyFlags
 	Output            string
-	CaCrt             string
-	ProxyCrt          string
-	ProxyKey          string
-	IntermediateCAs   []string
-	CaKey             string
-	CaPassword        string
-	CNames            []string
-	Country           string
-	State             string
-	City              string
-	Org               string
-	OrgUnit           string
-	SslEmail          string
+	Ssl               proxyConfigSslFlags
 }
 
 // proxyCreateConfigRequiredFields is a set of required fields for validation.
-var proxyCreateConfigRequiredFields = [6]string{
+var proxyCreateConfigRequiredFields = []string{
 	proxyName,
 	server,
 	email,
 	caCrt,
 }
 
-// NewConfigCommand creates the command for managing cache.
-// Setup for subcommand to clear (the cache).
-func NewConfigCommand(globalFlags *types.GlobalFlags) *cobra.Command {
-	var flags proxyCreateConfigFlags
-
+func newCmd(globalFlags *types.GlobalFlags, run utils.CommandFunc[proxyCreateConfigFlags]) *cobra.Command {
 	createConfigCmd := &cobra.Command{
 		Use:   "config",
 		Short: L("Create a proxy configuration file"),
 		Long:  L("Create a proxy configuration file"),
 		Example: `  Create a proxy configuration file providing certificates providing only required parameters
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@example.com" --caCrt="root_ca.pem" --proxyCrt="proxy_crt.pem" \
-		--proxyKey="proxy_key.pem"
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@example.com" --ssl-ca-cert="root_ca.pem" --ssl-proxy-cert="proxy_crt.pem" \
+		--ssl-proxy-key="proxy_key.pem"
 
   Create a proxy configuration file providing certificates providing  all parameters
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@example.com" --caCrt="root_ca.pem" --proxyCrt="proxy_crt.pem" \
-		--proxyKey="proxy_key.pem" --intermediateCAs="intermediateCA_1.pem,intermediateCA_2.pem,intermediateCA_3.pem" \
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@example.com" --ssl-ca-cert="root_ca.pem" --ssl-proxy-cert="proxy_crt.pem" \
+		--ssl-proxy-key="proxy_key.pem" \
+		--ssl-ca-intermediate="intermediateCA_1.pem,intermediateCA_2.pem,intermediateCA_3.pem" \
 		-o="proxy-config"
 
   or an alternative format:
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@example.com" --caCrt="root_ca.pem" --proxyCrt="proxy_crt.pem" \
-		--proxyKey="proxy_key.pem" --intermediateCAs "intermediateCA_1.pem" --intermediateCAs "intermediateCA_2.pem" \
-		--intermediateCAs "intermediateCA_3.pem" -o="proxy-config"
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@example.com" --ssl-ca-cert="root_ca.pem" --ssl-proxy-cert="proxy_crt.pem" \
+		--ssl-proxy-key="proxy_key.pem" \
+		--ssl-ca-intermediate "intermediateCA_1.pem" --ssl-ca-intermediate "intermediateCA_2.pem" \
+		--ssl-ca-intermediate "intermediateCA_3.pem" -o="proxy-config"
 
   Create a proxy configuration file with generated certificates providing only required parameters
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@org.com" --caCrt="ca.pem" --caKey="caKey.pem"
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@org.com" --ssl-ca-cert="ca.pem" --ssl-ca-key="caKey.pem"
 
   Create a proxy configuration file with generated certificates providing only required parameters and ca password
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@org.com" --caCrt="ca.pem" --caKey="caKey.pem" --caPassword="pass.txt"
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@org.com" --ssl-ca-cert="ca.pem" --ssl-ca-key="caKey.pem" --ssl-ca-password="pass.txt"
 
   Create a proxy configuration file with generated certificates providing all parameters
 
-    $ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@org.com" --caCrt="ca.pem" --caKey="caKey.pem" --caPassword="pass.txt" \
-		--cnames="proxy_a.example.com,proxy_b.example.com,proxy_c.example.com" --country="DE" \
-		--state="Bayern" --city="Nuernberg" --org="orgExample" --orgUnit="orgUnitExample" \
-		--sslEmail="sslEmail@example.com" -o="proxy-config"
+    $ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@org.com" --ssl-ca-cert="ca.pem" --ssl-ca-key="caKey.pem" --ssl-ca-password="pass.txt" \
+		--ssl-cnames="proxy_a.example.com,proxy_b.example.com,proxy_c.example.com" --ssl-country="DE" \
+		--ssl-state="Bayern" --ssl-city="Nuernberg" --ssl-org="orgExample" --ssl-ou="orgUnitExample" \
+		--ssl-email="sslEmail@example.com" -o="proxy-config"
 
   or an alternative format:
 
-	$ mgrctl proxy create config --proxyName="proxy.example.com" --server="server.example.com" \
-		--email="admin@org.com" --caCrt="ca.pem" --caKey="caKey.pem" --caPassword="pass.txt" \
-		--cnames="proxy_a.example.com" --cnames="proxy_b.example.com" --cnames="proxy_c.example.com" \
-		--country="DE" --state="Bayern" --city="Nuernberg" --org="orgExample" --orgUnit="orgUnitExample" \
-		--sslEmail="sslEmail@example.com" -o="proxy-config"`,
+	$ mgrctl proxy create config --proxy-name="proxy.example.com" --proxy-parent="server.example.com" \
+		--proxy-email="admin@org.com" --ssl-ca-cert="ca.pem" --ssl-ca-key="caKey.pem" --ssl-ca-password="pass.txt" \
+		--ssl-cnames="proxy_a.example.com" --ssl-cnames="proxy_b.example.com" --ssl-cnames="proxy_c.example.com" \
+		--ssl-country="DE" --ssl-state="Bayern" --ssl-city="Nuernberg" --ssl-org="orgExample" --ssl-ou="orgUnitExample" \
+		--ssl-email="sslEmail@example.com" -o="proxy-config"`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return utils.CommandHelper(globalFlags, cmd, args, &flags, nil, proxyCreateConfigInit)
+			var flags proxyCreateConfigFlags
+			return utils.CommandHelper(globalFlags, cmd, args, &flags, nil, run)
 		},
 	}
 
@@ -134,6 +132,12 @@ func NewConfigCommand(globalFlags *types.GlobalFlags) *cobra.Command {
 	createConfigCmd.MarkFlagsMutuallyExclusive(proxyCrt, caKey)
 
 	return createConfigCmd
+}
+
+// NewConfigCommand creates the command for managing cache.
+// Setup for subcommand to clear (the cache).
+func NewConfigCommand(globalFlags *types.GlobalFlags) *cobra.Command {
+	return newCmd(globalFlags, proxyCreateConfigInit)
 }
 
 func addFlags(cmd *cobra.Command) {
@@ -150,25 +154,19 @@ func addFlags(cmd *cobra.Command) {
 	// Specific flags for when providing proxy certificates
 	cmd.Flags().String(proxyCrt, "", L("Path to the proxy certificate in PEM format."))
 	cmd.Flags().String(proxyKey, "", L("Path to the proxy certificate private key in PEM format."))
-	cmd.Flags().StringSliceP(intermediateCAs, "i", []string{},
+	cmd.Flags().StringSliceP(caIntermediate, "i", []string{},
 		L(`Path to an intermediate CA used to sign the proxy certicate in PEM format.
 May be provided multiple times or separated by commas.`),
 	)
 
 	// Specific flags for when generating proxy certificates
+	ssl.AddSSLGenerationFlags(cmd)
+	cmd.Flags().String(sslEmail, "", L("Email to set in the SSL certificate"))
+
 	cmd.Flags().String(caKey, "", L("Path to the private key of the CA to use to generate a new proxy certificate."))
 	cmd.Flags().String(caPassword, "",
 		L("Path to a file containing the password of the CA private key, will be prompted if not passed."),
 	)
-	cmd.Flags().StringSlice(cNames, []string{},
-		L("Alternate name of the proxy to set in the certificate. May be provided multiple times or separated by commas."),
-	)
-	cmd.Flags().String(country, "", L("Country code to set in the certificate."))
-	cmd.Flags().String(state, "", L("State name to set in the certificate."))
-	cmd.Flags().String(city, "", L("City name to set in the certificate."))
-	cmd.Flags().String(org, "", L("Organization name to set in the certificate."))
-	cmd.Flags().String(orgUnit, "", L("Organization unit name to set in the certificate."))
-	cmd.Flags().String(sslEmail, "", L("Email to set in the certificate."))
 
 	// Login API flags
 	api.AddAPIFlags(cmd)
@@ -176,16 +174,13 @@ May be provided multiple times or separated by commas.`),
 	// Setup flag groups
 	commonGroup := "common"
 	providingGroup := "providing"
-	generateGroup := "generate"
 	_ = utils.AddFlagHelpGroup(cmd,
 		&utils.Group{ID: commonGroup, Title: L("Common Flags")},
-		&utils.Group{ID: providingGroup, Title: L("Provide proxy certificates flags")},
-		&utils.Group{ID: generateGroup, Title: L("Generate proxy certificates flags")},
+		&utils.Group{ID: providingGroup, Title: L("Third party proxy certificates flags")},
 	)
 
 	_ = utils.AddFlagsToHelpGroupID(cmd, commonGroup, proxyName, proxyPort, server, maxCache, email, caCrt)
-	_ = utils.AddFlagsToHelpGroupID(cmd, providingGroup, proxyCrt, proxyKey, intermediateCAs)
-	_ = utils.AddFlagsToHelpGroupID(
-		cmd, generateGroup, caKey, caPassword, cNames, country, state, city, org, orgUnit, sslEmail,
-	)
+	_ = utils.AddFlagsToHelpGroupID(cmd, providingGroup, proxyCrt, proxyKey, caIntermediate)
+
+	_ = utils.AddFlagsToHelpGroupID(cmd, "ssl", sslEmail)
 }
---
+++ mgrctl/cmd/proxy/config_test.go
@@ -0,0 +1,128 @@
+// SPDX-FileCopyrightText: 2024 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package proxy
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/uyuni-project/uyuni-tools/shared/testutils"
+	"github.com/uyuni-project/uyuni-tools/shared/testutils/flagstests"
+	"github.com/uyuni-project/uyuni-tools/shared/types"
+)
+
+func getCommonArgs() []string {
+	args := []string{
+		"--proxy-name", "pxy1.test.com",
+		"--proxy-sshPort", "1234",
+		"--proxy-parent", "uyuni.test.com",
+		"--proxy-maxCache", "123456",
+		"--proxy-email", "admin@proxy.test.com",
+		"--output", "path/to/output.tgz",
+		"--ssl-ca-cert", "path/to/ca.crt",
+	}
+	args = append(args, flagstests.APIFlagsTestArgs...)
+	return args
+}
+
+func assertCommonArgs(t *testing.T, cmd *cobra.Command, flags *proxyCreateConfigFlags) {
+	flagstests.AssertAPIFlags(t, cmd, &flags.ConnectionDetails)
+	testutils.AssertEquals(t, "Unexpected proxy name", "pxy1.test.com", flags.Proxy.Name)
+	testutils.AssertEquals(t, "Unexpected proxy SSH port", 1234, flags.Proxy.Port)
+	testutils.AssertEquals(t, "Unexpected proxy parent", "uyuni.test.com", flags.Proxy.Parent)
+	testutils.AssertEquals(t, "Unexpected proxy max cache", 123456, flags.Proxy.MaxCache)
+	testutils.AssertEquals(t, "Unexpected proxy email", "admin@proxy.test.com", flags.Proxy.Email)
+	testutils.AssertEquals(t, "Unexpected output path", "path/to/output.tgz", flags.Output)
+	testutils.AssertEquals(t, "Unexpected SSL CA cert path", "path/to/ca.crt", flags.Ssl.Ca.Cert)
+}
+
+func TestParamsParsingGeneratedCert(t *testing.T) {
+	args := getCommonArgs()
+	args = append(args,
+		"--ssl-ca-cert", "path/to/ca.crt",
+		"--ssl-ca-key", "path/to/ca.key",
+		"--ssl-ca-password", "path/to/ca.pass",
+		"--ssl-email", "ssl@test.com",
+	)
+	args = append(args, flagstests.SSLGenerationFlagsTestArgs...)
+
+	conflictingFlags := []string{
+		"--ssl-proxy-cert",
+		"--ssl-proxy-key",
+		"--ssl-ca-intermediate",
+	}
+
+	// Test function asserting that the args are properly parsed
+	tester := func(globalFlags *types.GlobalFlags, flags *proxyCreateConfigFlags,
+		cmd *cobra.Command, args []string,
+	) error {
+		assertCommonArgs(t, cmd, flags)
+		flagstests.AssertSSLGenerationFlags(t, cmd, &flags.Ssl.SslCertGenerationFlags)
+		testutils.AssertEquals(t, "Unexpected SSL CA cert path", "path/to/ca.crt", flags.Ssl.Ca.Cert)
+		testutils.AssertEquals(t, "Unexpected SSL CA key path", "path/to/ca.key", flags.Ssl.Ca.Key)
+		testutils.AssertEquals(t, "Unexpected SSL CA password path", "path/to/ca.pass", flags.Ssl.Ca.Password)
+		testutils.AssertEquals(t, "Unexpected SSL email", "ssl@test.com", flags.Ssl.Email)
+		return nil
+	}
+
+	globalFlags := types.GlobalFlags{}
+	cmd := newCmd(&globalFlags, tester)
+
+	testutils.AssertHasAllFlagsIgnores(t, cmd, args, conflictingFlags)
+
+	t.Logf("flags: %s", strings.Join(args, " "))
+	cmd.SetArgs(args)
+	if err := cmd.Execute(); err != nil {
+		t.Errorf("command failed with error: %s", err)
+	}
+}
+
+func TestParamsParsingProvidedCert(t *testing.T) {
+	args := getCommonArgs()
+	args = append(args,
+		"--ssl-ca-intermediate", "path/to/ca1.crt",
+		"--ssl-ca-intermediate", "path/to/ca2.crt",
+		"--ssl-proxy-cert", "path/to/proxy.crt",
+		"--ssl-proxy-key", "path/to/proxy.key",
+	)
+
+	conflictingFlags := []string{
+		"--ssl-email",
+		"--ssl-ca-key",
+		"--ssl-ca-password",
+		"--ssl-cname",
+		"--ssl-country",
+		"--ssl-state",
+		"--ssl-city",
+		"--ssl-org",
+		"--ssl-ou",
+	}
+
+	// Test function asserting that the args are properly parsed
+	tester := func(globalFlags *types.GlobalFlags, flags *proxyCreateConfigFlags,
+		cmd *cobra.Command, args []string,
+	) error {
+		assertCommonArgs(t, cmd, flags)
+		testutils.AssertEquals(t, "Unexpected SSL CA cert path", "path/to/ca.crt", flags.Ssl.Ca.Cert)
+		testutils.AssertEquals(t, "Unexpected SSL intermediate CA cert paths",
+			[]string{"path/to/ca1.crt", "path/to/ca2.crt"}, flags.Ssl.Ca.Intermediate,
+		)
+		testutils.AssertEquals(t, "Unexpected Proxy SSL cert path", "path/to/proxy.crt", flags.Ssl.Proxy.Cert)
+		testutils.AssertEquals(t, "Unexpected Proxy SSL key path", "path/to/proxy.key", flags.Ssl.Proxy.Key)
+		return nil
+	}
+
+	globalFlags := types.GlobalFlags{}
+	cmd := newCmd(&globalFlags, tester)
+
+	testutils.AssertHasAllFlagsIgnores(t, cmd, args, conflictingFlags)
+
+	t.Logf("flags: %s", strings.Join(args, " "))
+	cmd.SetArgs(args)
+	if err := cmd.Execute(); err != nil {
+		t.Errorf("command failed with error: %s", err)
+	}
+}
