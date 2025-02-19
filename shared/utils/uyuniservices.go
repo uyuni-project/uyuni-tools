9b947c48ab9201c561d40d9437174d3c554f8b12
Ondrej Holecek <oholecek@suse.com>
2025-02-19 17:05:46 +0100

    Add generic service information data
    Commit adds types and initial information about services we configure.

---
+++ shared/utils/uyuniservices.go
@@ -0,0 +1,83 @@
+// SPDX-FileCopyrightText: 2025 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package utils
+
+import (
+	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
+	"github.com/uyuni-project/uyuni-tools/shared/types"
+)
+
+// Server
+
+// Services
+var UyuniServices = []types.UyuniService{
+	{Name: "uyuni-server",
+		Image:       ServerImage,
+		Description: "",
+		Replicas:    types.SingleMandatoryReplica,
+		Options:     []types.UyuniServiceOption{}},
+
+	{Name: "uyuni-server-migration",
+		Image:       Migration14To16Image,
+		Description: "",
+		Replicas:    types.SingleOptionalReplica,
+		Options:     []types.UyuniServiceOption{}},
+
+	{Name: "uyuni-server-attestation",
+		Image:       COCOAttestationImage,
+		Description: L("confidential computing attestation"),
+		Replicas:    types.SingleOptionalReplica,
+		Options:     []types.UyuniServiceOption{}},
+
+	{Name: "uyuni-hub-xmlrpc",
+		Image:       HubXMLRPCImage,
+		Description: L("Hub XML-RPC API"),
+		Replicas:    types.SingleOptionalReplica,
+		Options:     []types.UyuniServiceOption{}},
+
+	{Name: "uyuni-server-saline",
+		Image:       SalineImage,
+		Description: L("Saline"),
+		Replicas:    types.SingleOptionalReplica,
+		Options:     []types.UyuniServiceOption{}},
+}
+
+// Images
+var ServerImage = types.ImageFlags{
+	Name:       "server",
+	Tag:        DefaultTag,
+	Registry:   DefaultRegistry,
+	PullPolicy: DefaultPullPolicy,
+}
+
+var HubXMLRPCImage = types.ImageFlags{
+	Name:       "server-hub-xmlrpc-api",
+	Tag:        DefaultTag,
+	Registry:   DefaultRegistry,
+	PullPolicy: DefaultPullPolicy,
+}
+
+var COCOAttestationImage = types.ImageFlags{
+	Name:       "erver-attestation",
+	Tag:        DefaultTag,
+	Registry:   DefaultRegistry,
+	PullPolicy: DefaultPullPolicy,
+}
+
+var SalineImage = types.ImageFlags{
+	Name:       "server-saline",
+	Tag:        DefaultTag,
+	Registry:   DefaultRegistry,
+	PullPolicy: DefaultPullPolicy,
+}
+
+var Migration14To16Image = types.ImageFlags{
+	Name:       "server-migration-14-16",
+	Tag:        DefaultTag,
+	Registry:   DefaultRegistry,
+	PullPolicy: DefaultPullPolicy,
+}
+
+// Proxy
