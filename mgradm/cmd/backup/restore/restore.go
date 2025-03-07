6edc9e6f1b5ff7828272eb08452c4d6729a21b82
Ondrej Holecek <oholecek@suse.com>
2025-03-07 15:43:14 +0100

    Add initial restore implementation

---
+++ mgradm/cmd/backup/restore/restore.go
@@ -0,0 +1,215 @@
+// SPDX-FileCopyrightText: 2025 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package restore
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"slices"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/uyuni-project/uyuni-tools/mgradm/cmd/backup/shared"
+	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
+	"github.com/uyuni-project/uyuni-tools/shared/podman"
+	"github.com/uyuni-project/uyuni-tools/shared/types"
+	"github.com/uyuni-project/uyuni-tools/shared/utils"
+)
+
+var systemd podman.Systemd = podman.SystemdImpl{}
+var runCmdOutput = utils.RunCmdOutput
+var runCmdInput = utils.RunCmdInput
+var runCmd = utils.RunCmd
+
+func Restore(
+	_ *types.GlobalFlags,
+	flags *shared.Flagpole,
+	_ *cobra.Command,
+	args []string,
+
+) error {
+	inputDirectory := args[0]
+	dryRun := flags.DryRun
+	// SanityCheck
+	if err := SanityChecks(inputDirectory, flags); err != nil {
+		return shared.AbortError(err, false)
+	}
+
+	volumes, err := gatherVolumesToRestore(inputDirectory, flags)
+	if err != nil {
+		return shared.AbortError(err, false)
+	}
+	images, err := gatherImagesToRestore(inputDirectory, flags)
+	if err != nil {
+		return shared.AbortError(err, false)
+	}
+	// TODO Check backup integrity
+
+	// Restore volumes if provided
+	if err := restoreVolumes(volumes, flags, dryRun); err != nil {
+		return shared.AbortError(err, true)
+	}
+
+	var hasError error
+	// Restore images if provided
+	if err := restoreImages(images, dryRun); err != nil {
+		hasError = err
+	}
+
+	// Restore podman config or generate defaults
+	if err := restorePodmanConfig(inputDirectory, flags); err != nil {
+		hasError = errors.Join(hasError, err)
+	}
+	// Restore systemd config or generate defaults
+	if err := restoreSystemdConfig(inputDirectory, flags); err != nil {
+		hasError = errors.Join(hasError, err)
+		// recreate services defaults
+
+	}
+	return shared.ReportError(hasError)
+}
+
+func SanityChecks(inputDirectory string, flags *shared.Flagpole) error {
+	if err := shared.SanityChecks(); err != nil {
+		return err
+	}
+
+	if !utils.FileExists(inputDirectory) {
+		return fmt.Errorf(L("input directory %s does not exists"), inputDirectory)
+
+	}
+
+	hostData, err := podman.InspectHost()
+	if err != nil {
+		return err
+	}
+
+	if hostData.HasUyuniServer {
+		if flags.ForceRestore {
+			log.Warn().Msg(L("Restoring over already initialized server"))
+		} else {
+			return errors.New(L("server is already initialized. Use force to overwrite"))
+		}
+
+	}
+	// TODO check if images from backup are existing
+
+	return nil
+}
+
+func gatherVolumesToRestore(source string, flags *shared.Flagpole) ([]string, error) {
+	skipVolumes := flags.SkipVolumes
+	if len(skipVolumes) == 1 && skipVolumes[0] == "all" {
+		log.Debug().Msg("Skipping restoring of volumes")
+		return []string{}, nil
+	}
+
+	volumeDir := path.Join(source, "volumes")
+	if !utils.FileExists(volumeDir) {
+		return []string{}, errors.New(L("No volumes found in the backup"))
+	}
+	volumes, err := os.ReadDir(volumeDir)
+	if err != nil {
+		return nil, errors.New(L("Unable to read directory with the volumes"))
+	}
+
+	output := []string{}
+	for _, v := range volumes {
+		volName, _ := strings.CutSuffix(v.Name(), ".tar")
+		if slices.Contains(skipVolumes, volName) {
+			log.Info().Msgf(L("Not restoring existing volume %s"), volName)
+			continue
+		}
+		if err := runCmd("podman", "volume", "exists", volName); err == nil {
+			if flags.SkipExisting {
+				log.Info().Msgf(L("Not restoring existing volume %s"), volName)
+				continue
+			}
+			if !flags.ForceRestore {
+				return nil, errors.New(L("Volume to restore already exists."))
+			}
+			log.Info().Msgf(L("Volume %s will be overwriten"), volName)
+		}
+		output = append(output, volName)
+	}
+	return output, nil
+}
+
+func gatherImagesToRestore(source string, flags *shared.Flagpole) ([]string, error) {
+	if flags.SkipImages {
+		log.Debug().Msg("Skipping restoring of images")
+		return []string{}, nil
+	}
+
+	imagesDir := path.Join(source, "images")
+	if !utils.FileExists(imagesDir) {
+		return []string{}, errors.New(L("No images found in the backup"))
+	}
+	images, err := os.ReadDir(imagesDir)
+	if err != nil {
+		return nil, errors.New(L("Unable to read directory with the images"))
+	}
+
+	output := []string{}
+	for _, image := range images {
+		imageName, _ := strings.CutSuffix(image.Name(), ".tar")
+		output = append(output, imageName)
+	}
+	return output, nil
+}
+
+func restoreVolumes(volumes []string, flags *shared.Flagpole, dryRun bool) error {
+	var hasError error
+	for _, volume := range volumes {
+		if err := podman.ImportVolume(volume, volume, flags.SkipVerify, dryRun); err != nil {
+			hasError = errors.Join(hasError, err)
+		}
+	}
+	return hasError
+}
+
+func restoreImages(images []string, dryRun bool) error {
+	var hasErrors error
+	for _, image := range images {
+		if err := podman.RestoreImage(image, dryRun); err != nil {
+			hasErrors = errors.Join(hasErrors, err)
+		}
+	}
+	return hasErrors
+}
+
+func restorePodmanConfig(inputDirectory string, flags *shared.Flagpole) error {
+	podmanConfigFile := path.Join(inputDirectory, shared.PodmanConfBackupFile)
+	if !utils.FileExists(podmanConfigFile) {
+		log.Warn().Msg(L("podman config backup not found in the backup location, trying defaults"))
+		return defaultPodmanNetwork(flags)
+	}
+
+	if !flags.SkipVerify {
+		if err := utils.ValidateChecksum(podmanConfigFile); err != nil {
+			return errors.Join(err, errors.New(L("Unable to validate podman backup file")))
+		}
+	}
+
+	return restorePodmanConfiguration(podmanConfigFile, flags)
+}
+
+func restoreSystemdConfig(inputDirectory string, flags *shared.Flagpole) error {
+	systemdConfigFile := path.Join(inputDirectory, shared.SystemdConfBackupFile)
+	if !utils.FileExists(systemdConfigFile) {
+		return errors.New(L("systemd backup not found in the backup location"))
+	}
+	if !flags.SkipVerify {
+		if err := utils.ValidateChecksum(systemdConfigFile); err != nil {
+			return errors.Join(err, errors.New(L("Unable to validate systemd backup file")))
+		}
+	}
+
+	return nil
+}
