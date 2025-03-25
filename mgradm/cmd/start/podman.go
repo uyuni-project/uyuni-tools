43c178827eb354155476cfd8a60c7527bbc4fbd7
Ondrej Holecek <oholecek@suse.com>
2025-03-25 14:05:20 +0100

    Refactor start and stop services to single place

--- mgradm/cmd/start/podman.go
+++ mgradm/cmd/start/podman.go
@@ -6,12 +6,9 @@ package start
 
 import (
 	"github.com/spf13/cobra"
-	"github.com/uyuni-project/uyuni-tools/shared/podman"
+	"github.com/uyuni-project/uyuni-tools/mgradm/shared/podman"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
-	"github.com/uyuni-project/uyuni-tools/shared/utils"
 )
-
-var systemd podman.Systemd = podman.SystemdImpl{}
 
 func podmanStart(
 	_ *types.GlobalFlags,
@@ -19,10 +16,5 @@ func podmanStart(
 	_ *cobra.Command,
 	_ []string,
 ) error {
-	return utils.JoinErrors(
-		systemd.StartService(podman.DBService),
-		systemd.StartInstantiated(podman.ServerAttestationService),
-		systemd.StartInstantiated(podman.HubXmlrpcService),
-		systemd.StartService(podman.ServerService),
-	)
+	return podman.StartServices()
 }
