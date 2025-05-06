bf382a65bd8b1215615fcc56901be85fa4b4c42d
Cédric Bosdonnat <cbosdonnat@suse.com>
2025-05-06 17:48:23 +0200

    Clean up the host supportconfig files (bsc#1242174)
    Before ending the command we need to remove the supportconfig file of
    the host in /var/log: this is a source of confusion for users.

--- mgradm/cmd/support/config/extractor.go
+++ mgradm/cmd/support/config/extractor.go
@@ -5,15 +5,27 @@
 package config
 
 import (
+	"os"
+
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/uyuni-project/uyuni-tools/shared"
 	"github.com/uyuni-project/uyuni-tools/shared/kubernetes"
+	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
 	"github.com/uyuni-project/uyuni-tools/shared/podman"
 	"github.com/uyuni-project/uyuni-tools/shared/types"
 	"github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 var systemd podman.Systemd = podman.SystemdImpl{}
+
+func filesRemover(files []string) {
+	for _, file := range files {
+		if err := os.Remove(file); err != nil {
+			log.Error().Err(err).Msgf(L("failed to remove %s temporary file"), file)
+		}
+	}
+}
 
 func extract(_ *types.GlobalFlags, flags *configFlags, _ *cobra.Command, _ []string) error {
 	containerName, err := shared.ChooseObjPodmanOrKubernetes(systemd, podman.ServerContainerName, kubernetes.ServerApp)
@@ -39,6 +51,7 @@ func extract(_ *types.GlobalFlags, flags *configFlags, _ *cobra.Command, _ []str
 	if systemd.HasService(podman.ServerService) {
 		fileListHost, err = podman.RunSupportConfigOnPodmanHost(systemd, tmpDir)
 	}
+	defer filesRemover(fileListHost)
 	if err != nil {
 		return err
 	}
