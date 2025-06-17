4ec7cce67cabbaadc2296b263223c7f6251855df
Cédric Bosdonnat <cbosdonnat@suse.com>
2025-06-17 18:53:17 +0200

    Extract the Runner as an interface for testing
    In order to use a mocked Runner, we need an interface. The Runner
    implementation is basically not needed by anyone. Tests can then use the
    FakeRunnerGenerator to create NewRunner functions.

---
+++ shared/testutils/runner.go
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: 2025 SUSE LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package testutils
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/uyuni-project/uyuni-tools/shared/types"
+)
+
+type fakeRunner struct {
+	out []byte
+	err error
+}
+
+func (r fakeRunner) Log(_ zerolog.Level) types.Runner {
+	return r
+}
+
+func (r fakeRunner) Spinner(_ string) types.Runner {
+	return r
+}
+
+func (r fakeRunner) StdMapping() types.Runner {
+	return r
+}
+
+func (r fakeRunner) Env(_ []string) types.Runner {
+	return r
+}
+
+func (r fakeRunner) Exec() ([]byte, error) {
+	return r.out, r.err
+}
+
+// FakeRunnerGenerator creates NewRunner function generating a FakeRunner.
+// out and err are the returns of the mocked Exec().
+func FakeRunnerGenerator(out string, err error) func(string, ...string) types.Runner {
+	return func(_ string, _ ...string) types.Runner {
+		runner := fakeRunner{
+			out: []byte(out),
+			err: err,
+		}
+		return runner
+	}
+}
