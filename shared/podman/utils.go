8ae100652a2c85b37abd311f8cb33524b53ec074
mihir-dixit2k27 <143348248+mihir-dixit2k27@users.noreply.github.com>
2026-01-18 03:41:37 +0530

    Add Name and Tag fields to PodmanFlags struct

--- shared/podman/utils.go
+++ shared/podman/utils.go
@@ -51,7 +51,9 @@ var ProxyContainerNames = []string{
 
 // PodmanFlags stores the podman arguments.
 type PodmanFlags struct {
-	Args []string `mapstructure:"arg"`
+    Args []string `mapstructure:"arg"`
+    Name string   `mapstructure:"server-image"`
+    Tag  string   `mapstructure:"server-tag"`
 }
 
 // GetCommonParams splits the common arguments.
