4857b5cbf8ccac79f2eb63047da09bb5512d2071
Digvijay Singh Rawat <hackerearthx1@gmail.com>
2026-02-02 07:47:49 +0000

    Add SSL certificate diagnostics to mgradm support config
    This adds SSL certificate information collection to help support engineers debug SSL-related issues. The following data is now included in the supportconfig tarball:
    
    - Certificate details (Subject, Issuer, Valid From/Until, CA status, Fingerprint) for Root CA, DB Root CA, and Server Certificate
    - Certificate chain validation results
    - Expiry warnings for certificates expiring within 30 days
    - Graceful handling of missing certificates

--- mgradm/cmd/support/config/extractor.go
+++ mgradm/cmd/support/config/extractor.go
@@ -5,64 +5,85 @@
 package config
 
 import (
-	"os"
+    "os"
 
-	"github.com/rs/zerolog/log"
-	"github.com/spf13/cobra"
-	"github.com/uyuni-project/uyuni-tools/shared"
-	"github.com/uyuni-project/uyuni-tools/shared/kubernetes"
-	. "github.com/uyuni-project/uyuni-tools/shared/l10n"
-	"github.com/uyuni-project/uyuni-tools/shared/podman"
-	"github.com/uyuni-project/uyuni-tools/shared/types"
-	"github.com/uyuni-project/uyuni-tools/shared/utils"
+    "github.com/rs/zerolog/log"
+    "github.com/spf13/cobra"
+    "github.com/uyuni-project/uyuni-tools/shared"
+    "github.com/uyuni-project/uyuni-tools/shared/kubernetes"
+    . "github.com/uyuni-project/uyuni-tools/shared/l10n"
+    "github.com/uyuni-project/uyuni-tools/shared/podman"
+    "github.com/uyuni-project/uyuni-tools/shared/ssl"
+    "github.com/uyuni-project/uyuni-tools/shared/types"
+    "github.com/uyuni-project/uyuni-tools/shared/utils"
 )
 
 var systemd podman.Systemd = podman.NewSystemd()
 
 func filesRemover(files []string) {
-	for _, file := range files {
-		if !utils.FileExists(file) {
-			log.Trace().Msgf("%s will not removed since it doesn't exists", file)
-			continue
-		}
-		if err := os.Remove(file); err != nil {
-			log.Error().Err(err).Msgf(L("failed to remove %s temporary file"), file)
-		}
-	}
+    for _, file := range files {
+        if !utils.FileExists(file) {
+            log.Trace().Msgf("%s will not removed since it doesn't exists", file)
+            continue
+        }
+        if err := os.Remove(file); err != nil {
+            log.Error().Err(err).Msgf(L("failed to remove %s temporary file"), file)
+        }
+    }
 }
 
 func extract(_ *types.GlobalFlags, flags *configFlags, _ *cobra.Command, _ []string) error {
-	containerName, err := shared.ChooseObjPodmanOrKubernetes(systemd, podman.ServerContainerName, kubernetes.ServerApp)
-	if err != nil {
-		return err
-	}
+    containerName, err := shared.ChooseObjPodmanOrKubernetes(systemd, podman.ServerContainerName, kubernetes.ServerApp)
+    if err != nil {
+        return err
+    }
 
-	cnx := shared.NewConnection(flags.Backend, containerName, kubernetes.ServerFilter)
+    cnx := shared.NewConnection(flags.Backend, containerName, kubernetes.ServerFilter)
 
 	// Copy the generated file locally
-	tmpDir, cleaner, err := utils.TempDir()
-	if err != nil {
-		return err
-	}
-	defer cleaner()
+    tmpDir, cleaner, err := utils.TempDir()
+    if err != nil {
+        return err
+    }
+    defer cleaner()
 
-	fileList, err := cnx.RunSupportConfig(tmpDir)
-	if err != nil {
-		return err
-	}
+    var fileList []string
+    var supportConfigErr error
 
-	var fileListHost []string
-	if systemd.HasService(podman.ServerService) {
-		fileListHost, err = podman.RunSupportConfigOnPodmanHost(systemd, tmpDir)
-	}
-	defer filesRemover(fileListHost)
-	if err != nil {
-		return err
-	}
+    // Run supportconfig but continue even if it fails
+    fileList, supportConfigErr = cnx.RunSupportConfig(tmpDir)
+    if supportConfigErr != nil {
+        log.Warn().Err(supportConfigErr).Msg(L("supportconfig failed, continuing with SSL collection"))
+    }
 
-	if len(fileListHost) > 0 {
-		fileList = append(fileList, fileListHost...)
-	}
+    // Collect SSL certificate information
+    sslInfoFile, sslErr := ssl.CollectSSLCertInfo(tmpDir, cnx.Exec)
+    if sslErr != nil {
+        log.Warn().Err(sslErr).Msg(L("failed to collect SSL certificate information"))
+    } else {
+        fileList = append(fileList, sslInfoFile)
+    }
 
-	return utils.CreateSupportConfigTarball(flags.Output, fileList)
+    var fileListHost []string
+    if systemd.HasService(podman.ServerService) {
+        fileListHost, err = podman.RunSupportConfigOnPodmanHost(systemd, tmpDir)
+    }
+    defer filesRemover(fileListHost)
+    if err != nil {
+        log.Warn().Err(err).Msg(L("failed to run supportconfig on host"))
+    }
+
+    if len(fileListHost) > 0 {
+        fileList = append(fileList, fileListHost...)
+    }
+
+    // If no files collected at all, return the original error
+    if len(fileList) == 0 {
+        if supportConfigErr != nil {
+            return supportConfigErr
+        }
+        return utils.Errorf(nil, "no files collected")
+    }
+
+    return utils.CreateSupportConfigTarball(flags.Output, fileList)
 }
