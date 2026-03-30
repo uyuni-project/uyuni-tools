8ff61124dca97f21a0dbebe6e6e6a7dbe4c49bca
Cédric Bosdonnat <cbosdonnat@suse.com>
2026-03-30 16:42:26 +0200

    Fix copyright check for the README comments

--- update-copyright-year.sh
+++ update-copyright-year.sh
@@ -15,11 +15,11 @@ for changed_file in $@; do
     esac
 
 
-    if ! grep -q "^[/#]\+ SPDX-FileCopyrightText: $current_year" $changed_file; then
-        lines=`grep "^[/#]\+ SPDX-FileCopyrightText: " $changed_file | wc -l`
+    if ! grep -q "^[/# ]*SPDX-FileCopyrightText: $current_year" $changed_file; then
+        lines=`grep "^[/# ]*SPDX-FileCopyrightText: " $changed_file | wc -l`
         if [ "z$lines" = "z1" ]; then
             echo "🛠️ Fixed copyright year on $changed_file"
-            sed -i -E "s/^([/#]+) SPDX-FileCopyrightText: [0-9]{4}/\1 SPDX-FileCopyrightText: $current_year/" $changed_file
+            sed -i -E "s/^([/# ]*)SPDX-FileCopyrightText: [0-9]{4}/\1SPDX-FileCopyrightText: $current_year/" $changed_file
             changed="y"
         else
             echo "🛑 Cannot update the copyright year in $changed_file (multiple lines)"
